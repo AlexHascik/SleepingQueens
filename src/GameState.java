dd773785bee3373d73c1bcbad0a4109d99da85c0
TheS4baton <hascikalex@gmail.com>
2022-11-28 21:16:51 +0100

    GameState

---
+++ src/GameState.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class GameState {
+
+    public int numberOfPlayers;
+    public int onTurn;
+    public Set<SleepingQueenPosition> sleepingQueens;
+    public Map<HandPosition, Optional<Card>> cards;
+    public Map<AwokenQueenPosition, Queen> awokenQueens;
+    public List<Card> cardsDiscardedLastTurn;
+}
--- src/Position.java
+++ src/Position.java
@@ -17,13 +17,13 @@ public class Position {
 //    }
 
     public Position(HandPosition handPosition){
-        this.handPosition = Optional.ofNullable(handPosition);
+        this.handPosition = Optional.of(handPosition);
     }
     public Position(SleepingQueenPosition sleepingQueenPosition){
-        this.sleepingQueenPosition = Optional.ofNullable(sleepingQueenPosition);
+        this.sleepingQueenPosition = Optional.of(sleepingQueenPosition);
     }
     public Position(AwokenQueenPosition awokenQueenPosition){
-        this.awokenQueenPosition = Optional.ofNullable(awokenQueenPosition);
+        this.awokenQueenPosition = Optional.of(awokenQueenPosition);
     }
 
     public Optional<HandPosition> getHandPosition() {
