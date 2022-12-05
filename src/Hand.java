636ddcf43a244c77fb578f8b546d206f79f61099
TheS4baton <hascikalex@gmail.com>
2022-12-06 00:19:12 +0100

    Hand implementation

--- src/Hand.java
+++ src/Hand.java
@@ -3,27 +3,43 @@ import java.util.*;
 public class Hand {
 
     private int playerIdx;
+    private List<Card> cards;
+    private List<Card> pickedCards;
 
     public Hand(int playerIdx){
         this.playerIdx = playerIdx;
+        cards = new ArrayList<>();
+        pickedCards = new ArrayList<>();
     }
 
     public Optional<List<Card>> pickCards(List<HandPosition> positions){
 
-
-        return null;
+        if(!positions.isEmpty()){
+            for(HandPosition position : positions){
+                pickedCards.add(cards.get(position.getCardIndex()));
+            }
+            return Optional.of(pickedCards);
+        }
+        return Optional.empty();
     }
     public Map<HandPosition, Card> removePickedCardsAndRedraw(){
         return null;
     }
     public void returnPickedCards(){
-
+        pickedCards.clear();
     }
     public HandPosition hasCardOfType(CardType type){
+
+        for(int i = 0; i < cards.size(); i++){
+            if(cards.get(i).getCardType() == type){
+                return new HandPosition(i, playerIdx);
+            }
+        }
+
         return null;
     }
     public List<Card> getCards(){
-        return null;
+        return cards;
     }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -7,33 +7,45 @@ public class Player {
     private EvaluateNumberedCards numberedCards = new EvaluateNumberedCards();
     private MoveQueen moveQueen = new MoveQueen();
 
+    private AwokenQueens awokenQueens;
+
 
     private Hand hand;
     public Player(int playerIdx){
         playerState = new PlayerState();
         hand = new Hand(playerIdx);
+        this.playerIdx = playerIdx;
+        awokenQueens = new AwokenQueens(this);
     }
     public void play(List<Position> cards){
+
+        // mozno evaluatnut karty, mame pocitat ze hrac zada dobre karty... ?
+
         ArrayList<Card> selectedCards = new ArrayList<>();
         Map<Integer, Optional<Card>> playerCards = playerState.getCards();
             Position cardPosition = cards.get(0);
             if(cardPosition instanceof HandPosition){
                 Card toBePlayed = playerCards.get(((HandPosition) cardPosition).getCardIndex()).get();
                 switch (toBePlayed.getCardType()){
-                    case KNIGHT: EvaluateAttack evaluateKnight = new EvaluateAttack(CardType.DRAGON);
-                        AwokenQueenPosition queen = (AwokenQueenPosition) cards.get(2);
-                        if(evaluateKnight.play(cards.get(2), queen.getPlayerIndex())){
+                    case KNIGHT:
+                        EvaluateAttack evaluateKnight = new EvaluateAttack(CardType.DRAGON);
+                        AwokenQueenPosition queen = (AwokenQueenPosition) cards.get(1);
+                        if(evaluateKnight.play(queen, queen.getPlayerIndex())){
                             //consume cards
                         }
-                    case SLEEPING_POTION: EvaluateAttack evaluateMagicWand = new EvaluateAttack(CardType.MAGIC_WAND);
-                        AwokenQueenPosition queenToSleep = (AwokenQueenPosition) cards.get(2);
-                        if(evaluateMagicWand.play(cards.get(2), queenToSleep.getPlayerIndex())) {
+                        break;
+                    case SLEEPING_POTION:
+                        EvaluateAttack evaluateMagicWand = new EvaluateAttack(CardType.MAGIC_WAND);
+                        AwokenQueenPosition queenToSleep = (AwokenQueenPosition) cards.get(1);
+                        if(evaluateMagicWand.play(queenToSleep, queenToSleep.getPlayerIndex())) {
                             //consume cards
                         }
+                        break;
                     case NUMBER:
 
                         if(numberedCards.play(hand.getCards())){
                             //consume cards
+
                         }
                     case KING:
                         moveQueen.play(cards.get(1));
